37ac66aa6d6eb319cd40f781223ad134de14a03a
dr353173-del <dr353173@gmail.com>
2026-06-24 13:32:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ from groq import Groq
 # ================= CONFIG =================
 
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-MODEL_NAME = "llama3-70b-8192"
-
+MODEL_NAME = "llama-3.3-70b-versatile"
 if not GROQ_API_KEY:
     raise ValueError("GROQ_API_KEY not set in environment variables")
 
